999b367d6312fb4d7b4f526b01c785a6dc9d4cf9
Yusuke Kubo <kubo.yusuke31@gmail.com>
2021-01-04 16:45:52 +0900

    Bootstrapの導入とデザインの調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: 'TestUser', email: 'test@example.com')
+14.times do |n|
+  name = Faker::Name.name
+  email = "test-#{n+1}@example.com"
+  User.create!(name: name, email: email)
+end
