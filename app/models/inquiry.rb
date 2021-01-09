30be9e0f985fa9b3a9fbe98c25fdd4bf975e7a61
Yusuke Kubo <26254464+yuskubo@users.noreply.github.com>
2021-01-09 17:02:16 +0900

    AWS SQSへのメッセージ登録処理を追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,12 @@
 class Inquiry < ApplicationRecord
+  before_save :downcase_email
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates_acceptance_of :agreement, allow_nil: false, message: 'プライバシーポリシーの同意が必要です', on: :create
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
