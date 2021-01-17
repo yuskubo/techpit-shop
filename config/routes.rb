1cf9a6e76221708c5e4f116f048a3a3c92d930d5
Yusuke Kubo <26254464+yuskubo@users.noreply.github.com>
2021-01-17 18:51:51 +0900

    メール送信を行うActive Jobとsidekiqを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :users
   resources :inquiries
+  if Rails.env.development?
+    require 'sidekiq/web'
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
