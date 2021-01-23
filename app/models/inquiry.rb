4064bf8d6ab8b27924dac3ef3df0684896cad7ad
Yusuke Kubo <26254464+yuskubo@users.noreply.github.com>
2021-01-23 18:17:08 +0900

    i18nによる日本語対応

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,7 +2,7 @@ class Inquiry < ApplicationRecord
   before_save :downcase_email
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
-  validates_acceptance_of :agreement, allow_nil: false, message: 'プライバシーポリシーの同意が必要です', on: :create
+  validates_acceptance_of :agreement, allow_nil: false, on: :create
 
   private
 
