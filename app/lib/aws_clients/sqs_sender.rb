33da7e029188eddee8ab8b8571b7ee20089b5466
Yusuke Kubo <26254464+yuskubo@users.noreply.github.com>
2021-01-23 11:52:56 +0900

    SQS登録クラス名の変更とメッセージ重複排除IDの追加

--- app/lib/aws_clients/sqs_sender.rb
+++ app/lib/aws_clients/sqs_sender.rb
@@ -1,5 +1,5 @@
 module AwsClients
-  class SqsRegister
+  class SqsSender
     def initialize(region, queue_url, body, attributes, group_id)
       @sqs = Aws::SQS::Client.new(region: region)
       @queue_url = queue_url
@@ -13,6 +13,7 @@ module AwsClients
         queue_url: @queue_url,
         message_body: @body,
         message_attributes: @attributes,
+        message_deduplication_id: SecureRandom.uuid,
         message_group_id: @group_id
       })
     rescue Aws::SQS::Errors::ServiceError => e
