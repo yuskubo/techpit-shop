40c33c7c5995a831d3401103b5a96e318967e693
Yusuke Kubo <kubo.yusuke31@gmail.com>
2021-01-23 20:48:26 +0900

    問い合わせ完了画面の追加

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -7,13 +7,19 @@ class InquiriesController < ApplicationController
     @inquire = Inquiry.new(inquiry_params)
     if @inquire.save
       body = 'Inquiry EMail'
-      attributes = { inquiry_id: { string_value: @inquire.id.to_s, data_type: 'Number' } }
+      attributes = {
+        inquiry_id: { string_value: @inquire.id.to_s, data_type: 'Number' },
+        email_subject: { string_value: '無料体験レッスンの日程のお知らせ', data_type: 'String' }
+      }
       group_id = 'inquiry_email'
-      sqs_register = AwsClients::SqsRegister.new('ap-northeast-1', Rails.application.credentials.queue_url, body, attributes, group_id)
-      sqs_register.send_message
+      sqs_sender = AwsClients::SqsSender.new('ap-northeast-1', Rails.application.credentials.queue_url, body, attributes, group_id)
+      sqs_sender.send_message
 
-      redirect_to root_path
+      redirect_to inquiries_complete_url
     end
+  end
+
+  def complete
   end
 
   private
