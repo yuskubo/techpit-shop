e803e4072a051d42d3efe605b574f92b862c3586
Yusuke Kubo <kubo.yusuke31@gmail.com>
2021-01-23 19:16:21 +0900

    inquiriesテーブルのnameカラムをfirst_nameとlast_nameへ分割 sqliteではカラム追加場所をmigrateでコントロールできないため、テーブル自体を作り直し

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,7 +2,8 @@ class Inquiry < ApplicationRecord
   before_save :downcase_email
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :last_name,  presence: true, length: { maximum: 50 }
+  validates :first_name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   validates_acceptance_of :agreement, allow_nil: false, on: :create
 
