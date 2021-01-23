2bfed341c339b8a78f1d09fbababdc9dd7cc207d
Yusuke Kubo <26254464+yuskubo@users.noreply.github.com>
2021-01-23 18:24:38 +0900

    メールアドレスの形式を確認するバリデーションを追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,7 +1,9 @@
 class Inquiry < ApplicationRecord
   before_save :downcase_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   validates_acceptance_of :agreement, allow_nil: false, on: :create
 
   private
