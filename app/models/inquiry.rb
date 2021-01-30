4f425705758d0a7ae4417689bb6d6b731fc08797
Yusuke Kubo <kubo.yusuke31@gmail.com>
2021-01-30 21:43:04 +0900

    バリデーションエラーで二重に出ていたメッセージを出ないように修正

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 class Inquiry < ApplicationRecord
   before_save :downcase_email
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :last_name,  presence: true, length: { maximum: 50 }
-  validates :first_name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  validates_acceptance_of :agreement, allow_nil: false, on: :create
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
+  validates :email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
+  validates :agreement, acceptance: true, allow_nil: false, on: :create
 
   private
 
