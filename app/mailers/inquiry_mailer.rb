88ca90c9b4d39f02906d5d23f926f91a663b6771
Yusuke Kubo <kubo.yusuke31@gmail.com>
2021-01-31 19:43:43 +0900

    不要なファイルを削除

--- app/mailers/inquiry_mailer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class InquiryMailer < ApplicationMailer
-  def inquiry(inquirer, subject)
-    @last_name = inquirer.last_name
-    mail(to: inquirer.email, subject: subject)
-  end
-end
